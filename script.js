57f6e25abc58992fc62abd3da5eae9e70cdead3d
Chue Chang <chuechang1996@outlook.com>
2022-01-12 20:04:11 -0600

    added date

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const apikey = "addb76e535c4e5e7659ab5807b934e3b"
+setInterval(function () {
+    var currentDisplayEl = moment().format("[Today is] dddd, MMMM Do YYYY");
+    document.getElementById('clock').textContent = currentDisplayEl
+}, 0)
 
 var getWeather = function (city) {
     var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=addb76e535c4e5e7659ab5807b934e3b`;
@@ -9,31 +12,38 @@ var getWeather = function (city) {
                 //this is the data object.
                 var lon = data[0].lon
                 var lat = data[0].lat
-
-                // console.log(lon, lat);
+                console.log(data);
                 return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=addb76e535c4e5e7659ab5807b934e3b`)
                     .then(function (response) {
                         return response.json().then(function (data) {
+                            //object data storing in variable called current
                             var current = {
                                 humidity: data.current.humidity,
                                 temp: data.current.temp,
                                 clouds: data.current.clouds,
                                 icon: data.current.weather[0].icon
                             }
-                            console.log(current,data);
-                            document.getElementById('degree').textContent = "Current Temp: " + Math.floor(current.temp) + "°" 
-                            document.getElementById('icon').setAttribute("src",`http://openweathermap.org/img/wn/${current.icon}@2x.png`)
-                            
-                            
+                            console.log(current, data);
+                            //adding current temp
+                            document.getElementById('degree').textContent = "Current Temp: " + Math.floor(current.temp) + "°"
+                            //adding weather icon to html
+                            document.getElementById('icon').setAttribute("src", `http://openweathermap.org/img/wn/${current.icon}@2x.png`)
+                            //adding city name
+                            var cityInput = document.getElementById("inputCity").value
+                            var nameOfCity = document.getElementById("cityName")
+                            nameOfCity.textContent = cityInput.toUpperCase()
+
+
                         })
                     })
             })
         })
 
 }
-// var displayResult = document.getElementById("display-result")
+
 var buttonEl = document.getElementById("submitBtn")
 buttonEl.addEventListener("click", function () {
+
     var search = document.getElementById("inputCity").value
     getWeather(search)
 })
